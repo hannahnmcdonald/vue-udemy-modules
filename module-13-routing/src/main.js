1442cfd90ba8bf1de14eacfb58f48b648f46f426
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-31 13:55:50 -0500

    import routes, and call createRouter() fx

--- module-13-routing/src/main.js
+++ module-13-routing/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue';
+// Import function to make router from vue-router
+import { createRouter } from 'vue-router';
 
 import App from './App.vue';
+
+// Call fx- but needs some clarification
+const router = createRouter({
+    // Register all the routes- ARRAY OF ROUTES
+    routes: []
+});
 
 const app = createApp(App)
 

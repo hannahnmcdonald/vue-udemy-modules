67ea09b1fb5a7ff0919dd703f4b01051e2564d7c
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-31 13:57:13 -0500

    createWebHistory() fx

--- module-13-routing/src/main.js
+++ module-13-routing/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 // Import function to make router from vue-router
-import { createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
 // Call fx- but needs some clarification
 const router = createRouter({
+    // Pull from hx
+    history: createWebHistory(),
     // Register all the routes- ARRAY OF ROUTES
     routes: []
 });

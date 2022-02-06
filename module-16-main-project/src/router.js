ffcfffdc6a3034efb0e0121a8086fa05bea7cfa5
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-02-06 09:36:08 -0500

    components added to routes

--- module-16-main-project/src/router.js
+++ module-16-main-project/src/router.js
@@ -1,17 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
+//import page components
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachReg from './pages/coaches/CoachRegistration.vue';
+import ReqRecieved from './pages/requests/RequestRecieved.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: '/contact', component: null } // nested route to contact coach via coach id
+        { path: '/coaches', component: CoachList },
+        { path: '/coaches/:id', 
+            component: CoachDetail, 
+            children: [
+                { path: '/contact', component: ContactCoach } // nested route to contact coach via coach id
         ] },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
+        { path: '/register', component: CoachReg },
+        { path: '/requests', component: ReqRecieved },
         // error page
-        { path: '/:notFound(.*)', component: null },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
